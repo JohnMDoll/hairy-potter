cd02f4e18d3c94cb680d1a5e865cded4921315e3
JohnMDoll <johnmdoll3@gmail.com>
2022-10-06 10:59:22 -0500

    modified main?

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 // Imports go first
 import {makePottery} from "./PotteryWheel.js" 
-
+import {firePottery} from "./Kiln.js"
 
 // Make 5 pieces of pottery at the wheel
 let mug = makePottery("mug", 4,6)
@@ -14,7 +14,16 @@ console.log(smallMug)
 let saucer = makePottery("saucer", 4,3)
 console.log(saucer)
 // Fire each piece of pottery in the kiln
-
+mug = firePottery(mug,2201)
+console.log(mug)
+plate = firePottery(plate,200)
+console.log(plate)
+bowl = firePottery(bowl,2200)
+console.log(bowl)
+smallMug = firePottery(smallMug,3200)
+console.log(smallMug)
+saucer = firePottery(saucer,2)
+console.log(saucer)
 
 // Determine which ones should be sold, and their price
 
